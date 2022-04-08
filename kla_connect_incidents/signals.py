ad568f6bd4d4efa23724f5ef5d9d85c4290d46f0
Ssemate Brian <bronej.jeffries2067@gmail.com>
2022-04-08 19:54:14 +0300

    modify signals

--- kla_connect_incidents/signals.py
+++ kla_connect_incidents/signals.py
@@ -5,13 +5,14 @@ from kla_connect_incidents.models import KlaConnectReport, KlaConnectIncident, \
     INCIDENT_STATUS_COMPLETE, get_user_model
 from kla_connect_utils.constants import FEEDBACK_CHANGE_VERB, REJECTED_VERB, \
     COMPLETED_VERB, CITIZEN_USER, INCIDENT_ALERT, REPORT_ALERT, INCIDENT_STATUS_FOR_REVIEW, \
-    MANAGER_TRANSPORT, REPORT_REVIEW_VERB, INCIDENT_REPORT_REJECTED
+    MANAGER_TRANSPORT, REPORT_REVIEW_VERB, INCIDENT_REPORT_REJECTED, UPDATED_VERB
 
 
 @receiver(post_save, sender=KlaConnectIncident)
 def handle_incident_update(sender, instance, created, **kwargs):
     if not created:
-        feedbackchanged = (instance.previous_feedback != instance.feedback)
+        feedbackchanged = (
+            instance.previous_feedback is not None and instance.previous_feedback != instance.feedback)
         if feedbackchanged:
             notify.send(
                 instance.author,
@@ -20,12 +21,12 @@ def handle_incident_update(sender, instance, created, **kwargs):
                 action_object=instance,
                 description="Added feedback to incident",
                 public=False,
-                author=instance.author.full_name if instance.author else "System Activity",
+                author=instance.author.full_name,
                 previous_feedback=instance.previous_feedback,
                 current_feedback=instance.feedback
             )
 
-        completed = (instance.previous_status != instance.status
+        completed = (instance.previous_status is not None and instance.previous_status != instance.status
                      and instance.status == INCIDENT_STATUS_COMPLETE)
         if completed:
             notify.send(
@@ -36,7 +37,7 @@ def handle_incident_update(sender, instance, created, **kwargs):
                 level="success",
                 description="Approved Incident",
                 public=False,
-                author=instance.author.full_name if instance.author else "System Activity",
+                author=instance.author.full_name,
                 previous_status=instance.previous_status,
                 current_status=instance.status
             )
@@ -52,11 +53,26 @@ def handle_incident_update(sender, instance, created, **kwargs):
                 description="Reported Incident"
             )
 
+        if not (completed and feedbackchanged):
+            notify.send(
+                instance.author,
+                recipient=instance.user,
+                verb=UPDATED_VERB,
+                action_object=instance,
+                level="success",
+                description="Updated Incident",
+                public=False,
+                author=instance.author.full_name if instance.author else "System Activity",
+                previous_status=instance.previous_status,
+                current_status=instance.status
+            )
+
 
 @receiver(post_save, sender=KlaConnectReport)
 def handle_report_update(sender, instance, created, **kwargs):
     if not created:
-        feedbackchanged = (instance.previous_feedback != instance.feedback)
+        feedbackchanged = (
+            instance.previous_feedback is not None and instance.previous_feedback != instance.feedback)
         if feedbackchanged:
             notify.send(
                 instance.author,
@@ -70,7 +86,7 @@ def handle_report_update(sender, instance, created, **kwargs):
                 current_feedback=instance.feedback
             )
 
-        forwarded_for_review = (instance.previous_status != instance.status
+        forwarded_for_review = (instance.previous_status is not None and instance.previous_status != instance.status
                                 and instance.status == INCIDENT_STATUS_FOR_REVIEW)
         if forwarded_for_review:
             notify.send(
@@ -86,7 +102,7 @@ def handle_report_update(sender, instance, created, **kwargs):
                 current_status=instance.status
             )
 
-        rejected_report = (instance.previous_status != instance.status
+        rejected_report = (instance.previous_status is not None and instance.previous_status != instance.status
                            and instance.status == INCIDENT_REPORT_REJECTED)
         if rejected_report:
             notify.send(
@@ -102,7 +118,7 @@ def handle_report_update(sender, instance, created, **kwargs):
                 current_status=instance.status
             )
 
-        completed = (instance.previous_status != instance.status
+        completed = (instance.previous_status is not None and instance.previous_status != instance.status
                      and instance.status == INCIDENT_STATUS_COMPLETE)
         if completed:
             notify.send(
@@ -117,7 +133,7 @@ def handle_report_update(sender, instance, created, **kwargs):
                 previous_status=instance.previous_status,
                 current_status=instance.status
             )
-        
+
         if instance.publishing:
             # alert all citiziens
             notify.send(
@@ -129,3 +145,16 @@ def handle_report_update(sender, instance, created, **kwargs):
                 public=False,
                 description=REPORT_ALERT
             )
+
+        else:
+            if not (feedbackchanged and forwarded_for_review and rejected_report and completed):
+                notify.send(
+                    instance.user,
+                    recipient=instance.user,
+                    verb=UPDATED_VERB,
+                    action_object=instance,
+                    level="success",
+                    public=False,
+                    description="Updated Report"
+                )
+
