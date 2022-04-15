322df88ee1e4c6a5e09a50b5dfd87354d7f52443
Ssemate Brian <bronej.jeffries2067@gmail.com>
2022-04-15 16:24:36 +0300

    change query

--- kla_connect_utils/views.py
+++ kla_connect_utils/views.py
@@ -18,6 +18,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
+from kla_connect_utils.constants import CITIZEN_USER, INCIDENT_STATUS_COMPLETE
 
 from kla_connect_utils.filterbackends import (DEFAULT_FILTER_BACKENDS,
                                               NotificationsFilterBackend)
@@ -80,7 +81,7 @@ class NotificationsViewSet(ListModelMixin, GenericViewSet):
 class DashboardView(APIView):
 
     permission_classes = [IsAuthenticated]
-    
+
     @method_decorator(cache_page(10*60))
     @method_decorator(vary_on_headers("Authorization",))
     def get(self, request, format=None):
@@ -106,14 +107,14 @@ class DashboardView(APIView):
             raise ValidationError(str(e))
 
     def get_users_count(self, start, end):
-        return get_user_model().objects.filter(date_joined__date__range=[start, end]).count()
+        return get_user_model().objects.filter(role=CITIZEN_USER, date_joined__date__range=[start, end]).count()
 
     def get_summary_data(self, start, end):
-        incidents_summary = KlaConnectIncident.objects.filter(created_on__date__range=[start, end]).annotate(
-            date=TruncDate('created_on')).values('date').annotate(count=Count('date')).order_by()
+        incidents_summary = KlaConnectIncident.objects.filter(status=INCIDENT_STATUS_COMPLETE, created_on__date__range=[start, end]).annotate(
+            date=TruncDate('created_on')).values('date').annotate(count=Count('id')).order_by()
 
-        reports_summary = KlaConnectReport.objects.filter(created_on__date__range=[start, end]).annotate(
-            date=TruncDate('created_on')).values('date').annotate(count=Count('date')).order_by()
+        reports_summary = KlaConnectReport.objects.filter(published=True, created_on__date__range=[start, end]).annotate(
+            date=TruncDate('created_on')).values('date').annotate(count=Count('id')).order_by()
 
         incidents_summary_dates = incidents_summary.values_list(
             "date", flat=True)
@@ -121,8 +122,8 @@ class DashboardView(APIView):
         list(incidents_summary_dates).extend(list(reports_summary_dates))
         dates = set(incidents_summary_dates)
         data = [[], []]
-        incidents_count = incidents_summary.count()
-        reports_count = reports_summary.count()
+        incidents_count = 0
+        reports_count = 0
         for filter_date in dates:
             incident_reported = incidents_summary.filter(
                 date=filter_date).first()
@@ -130,11 +131,13 @@ class DashboardView(APIView):
             if incident_reported:
                 value = incident_reported.get('count')
             data[0].append(value)
+            incidents_count += value
 
             report = reports_summary.filter(date=filter_date).first()
             report_value = 0
             if report:
                 report_value = report.get('count')
             data[1].append(report_value)
+            reports_count += report_value
 
-        return {'incidents': incidents_count, 'reports': reports_count, 'summary_chart': data,'labels':dates}
+        return {'incidents': incidents_count, 'reports': reports_count, 'summary_chart': data, 'labels': dates}
